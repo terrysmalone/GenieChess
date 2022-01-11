b2a4334f3b501ae8271fbc684e1ab7130435e8a9
Terry malone <terry.malone@red-gate.com>
2022-01-11 13:18:00 +0000

    Implemented directionboard tests

--- ChessEngineTests/LookupTablesTests.cs
+++ ChessEngineTests/LookupTablesTests.cs
@@ -1,4 +1,5 @@
-﻿using ChessEngine.BoardSearching;
+﻿using System;
+using ChessEngine.BoardSearching;
 using NUnit.Framework;
 
 namespace ChessEngineTests
@@ -33,6 +34,20 @@ namespace ChessEngineTests
             Assert.That(LookupTables.UpBoard[boardPosition], Is.EqualTo(expectedupBoard));
         }
 
+        [TestCase(0, 0u)]                     // a1
+        [TestCase(7, 0u)]                     // h1
+        [TestCase(56, 282578800148737u)]      // a8
+        [TestCase(63, 36170086419038336u)]    // h8
+        [TestCase(18, 1028u)]                 // c3
+        [TestCase(39, 2155905152u)]           // h5
+        [TestCase(20, 4112u)]                 // e3
+        public void DownDirectionBoards(int boardPosition, ulong expectedDownBoard)
+        {
+            LookupTables.InitialiseAllTables();
+
+            Assert.That(LookupTables.DownBoard[boardPosition], Is.EqualTo(expectedDownBoard));
+        }
+
         [TestCase(0, 254u)]                   // a1
         [TestCase(7, 0u)]                     // h1
         [TestCase(56, 18302628885633695744u)] // a8
@@ -40,32 +55,34 @@ namespace ChessEngineTests
         [TestCase(18, 16252928u)]             // c3
         [TestCase(39, 0u)]                    // h5
         [TestCase(20, 14680064u)]             // e3
-        public void RightDirectionBoards(int boardPosition, ulong expectedrightBoard)
+        public void RightDirectionBoards(int boardPosition, ulong expectedRightBoard)
         {
             LookupTables.InitialiseAllTables();
 
-            Assert.That(LookupTables.RightBoard[boardPosition], Is.EqualTo(expectedrightBoard));
+            Assert.That(LookupTables.RightBoard[boardPosition], Is.EqualTo(expectedRightBoard));
         }
 
-        //[Test]
-        //public void TestDownDirectionBoards()
-        //{
-        //    throw new NotImplementedException();
-        //}
+        [TestCase(0, 0u)]                    // a1
+        [TestCase(7, 127u)]                  // h1
+        [TestCase(56, 0u)]                   // a8
+        [TestCase(63, 9151314442816847872u)] // h8
+        [TestCase(18, 196608u)]              // c3
+        [TestCase(39, 545460846592u)]        // h5
+        [TestCase(20, 983040u)]              // e3
+        public void LeftDirectionBoards(int boardPosition, ulong expectedLeftBoard)
+        {
+            LookupTables.InitialiseAllTables();
 
-        //[Test]
-        //public void TestLeftDirectionBoards()
-        //{
-        //    throw new NotImplementedException();
-        //}
+            Assert.That(LookupTables.LeftBoard[boardPosition], Is.EqualTo(expectedLeftBoard));
+        }
 
         #region mask tests
 
-        //[Test]
-        //public void TestFileMasks()
-        //{
-        //    throw new NotImplementedException();
-        //}
+        [Test]
+        public void FileMasks()
+        {
+            throw new NotImplementedException();
+        }
 
         //[Test]
         //public void TestRankMasks()
