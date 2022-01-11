9a2298e2a1efeba5d2d9ce4c564d00b6fb755407
Terry malone <terry.malone@red-gate.com>
2022-01-11 09:43:00 +0000

    Up and right direction boards tested

--- ChessEngineTests/LookupTablesTests.cs
+++ ChessEngineTests/LookupTablesTests.cs
@@ -1,49 +1,59 @@
 ﻿using ChessEngine.BoardSearching;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 
 namespace ChessEngineTests
 {
-    [TestClass]
+    [TestFixture]
     public class LookupTablesTests
     {
-        //[TestMethod]
+        //[Test]
         //public void TestSquareValueFromIndexTable()
         //{
         //    throw new NotImplementedException();
         //}
 
-        //[TestMethod]
+        //[Test]
         //public void TestSquareValueFromPositionTable()
         //{
         //    throw new NotImplementedException();
         //}
 
-        [TestMethod]
-        public void TestUpDirectionBoards()
+        [TestCase(0, 72340172838076672u)]    // a1
+        [TestCase(7, 9259542123273814016u)]  // h1
+        [TestCase(56, 0u)]                   // a8
+        [TestCase(63, 0u)]                   // h8
+        [TestCase(3, 578721382704613376u)]   // d1
+        [TestCase(16, 72340172838010880u)]   // a3
+        [TestCase(61, 0u)]                   // f8
+        [TestCase(39, 9259541571362095104)]  // h5
+        public void UpDirectionBoards(int boardPosition, ulong expectedupBoard)
         {
             LookupTables.InitialiseAllTables();
 
-            Assert.AreEqual((ulong)578721382704613376, LookupTables.UpBoard[3]);     //d1
-            Assert.AreEqual((ulong)72340172838010880, LookupTables.UpBoard[16]);     //a3
-            Assert.AreEqual((ulong)0, LookupTables.UpBoard[61]);     //f8
-            Assert.AreEqual((ulong)9259541571362095104, LookupTables.UpBoard[39]);     //h5
+            Assert.That(LookupTables.UpBoard[boardPosition], Is.EqualTo(expectedupBoard));
         }
 
-        [TestMethod]
-        public void TestRightDirectionBoards()
+        [TestCase(0, 254u)]                   // a1
+        [TestCase(7, 0u)]                     // h1
+        [TestCase(56, 18302628885633695744u)] // a8
+        [TestCase(63, 0u)]                    // h8
+        [TestCase(18, 16252928u)]             // c3
+        [TestCase(39, 0u)]                    // h5
+        [TestCase(20, 14680064u)]             // e3
+        public void RightDirectionBoards(int boardPosition, ulong expectedrightBoard)
         {
             LookupTables.InitialiseAllTables();
 
-            Assert.AreEqual((ulong)16252928, LookupTables.RightBoard[18]);     //c3
+            Assert.That(LookupTables.RightBoard[boardPosition], Is.EqualTo(expectedrightBoard));
         }
 
-        //[TestMethod]
+        //[Test]
         //public void TestDownDirectionBoards()
         //{
         //    throw new NotImplementedException();
         //}
 
-        //[TestMethod]
+        //[Test]
         //public void TestLeftDirectionBoards()
         //{
         //    throw new NotImplementedException();
@@ -51,25 +61,25 @@ namespace ChessEngineTests
 
         #region mask tests
 
-        //[TestMethod]
+        //[Test]
         //public void TestFileMasks()
         //{
         //    throw new NotImplementedException();
         //}
 
-        //[TestMethod]
+        //[Test]
         //public void TestRankMasks()
         //{
         //    throw new NotImplementedException();
         //}
 
-        //[TestMethod]
+        //[Test]
         //public void TestFileMasksByColumn()
         //{
         //    throw new NotImplementedException();
         //}
 
-        //[TestMethod]
+        //[Test]
         //public void TestFileMasksByIndex()
         //{
         //    throw new NotImplementedException();
