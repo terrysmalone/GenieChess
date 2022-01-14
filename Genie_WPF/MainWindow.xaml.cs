a84d44a041a0cec0dd857e833ef4ecf8db2962f2
Terry malone <terry.malone@red-gate.com>
2022-01-14 17:01:44 +0000

    Start loading a board

--- Genie_WPF/MainWindow.xaml.cs
+++ Genie_WPF/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Media.Imaging;
+using ChessEngine.BoardRepresentation;
 using ChessEngine.NotationHelpers;
 
 namespace Genie_WPF
@@ -17,9 +18,11 @@ namespace Genie_WPF
 
             var boardState = FenTranslator.ToBoardState("5r1k/4Qpq1/4p3/1p1p2P1/2p2P2/1p2P3/3P4/BK6 b - -");
 
-            //var boardViewModel = new BoardViewModel(boardState);
+            var boardViewModel = new BoardViewModel(boardState);
 
-            Console.WriteLine("Hello World");
+
+            var whitePawnMoves = BitboardOperations.GetSquareIndexesFromBoardValue()
+
         }
     }
 }
