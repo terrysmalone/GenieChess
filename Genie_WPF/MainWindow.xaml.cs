3141438beea93478150dfd28ce95756710f3d376
Terry malone <terry.malone@red-gate.com>
2022-01-14 16:15:05 +0000

    I've got something that shows a grid

--- Genie_WPF/MainWindow.xaml.cs
+++ Genie_WPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.Windows;
+using System.Windows.Media.Imaging;
 
 namespace Genie_WPF
 {
@@ -12,41 +13,8 @@ namespace Genie_WPF
         {
             InitializeComponent();
 
-            ChessBoard.ItemsSource = new ObservableCollection<ChessPiece>
-            {
-                new ChessPiece{Pos=new Point(0, 6), Type=PieceType.Pawn, Player=Player.White},
-                new ChessPiece{Pos=new Point(1, 6), Type=PieceType.Pawn, Player=Player.White},
-                new ChessPiece{Pos=new Point(2, 6), Type=PieceType.Pawn, Player=Player.White},
-                new ChessPiece{Pos=new Point(3, 6), Type=PieceType.Pawn, Player=Player.White},
-                new ChessPiece{Pos=new Point(4, 6), Type=PieceType.Pawn, Player=Player.White},
-                new ChessPiece{Pos=new Point(5, 6), Type=PieceType.Pawn, Player=Player.White},
-                new ChessPiece{Pos=new Point(6, 6), Type=PieceType.Pawn, Player=Player.White},
-                new ChessPiece{Pos=new Point(7, 6), Type=PieceType.Pawn, Player=Player.White},
-                new ChessPiece{Pos=new Point(0, 7), Type=PieceType.Rook, Player=Player.White},
-                new ChessPiece{Pos=new Point(1, 7), Type=PieceType.Knight, Player=Player.White},
-                new ChessPiece{Pos=new Point(2, 7), Type=PieceType.Bishop, Player=Player.White},
-                new ChessPiece{Pos=new Point(3, 7), Type=PieceType.King, Player=Player.White},
-                new ChessPiece{Pos=new Point(4, 7), Type=PieceType.Queen, Player=Player.White},
-                new ChessPiece{Pos=new Point(5, 7), Type=PieceType.Bishop, Player=Player.White},
-                new ChessPiece{Pos=new Point(6, 7), Type=PieceType.Knight, Player=Player.White},
-                new ChessPiece{Pos=new Point(7, 7), Type=PieceType.Rook, Player=Player.White},
-                new ChessPiece{Pos=new Point(0, 1), Type=PieceType.Pawn, Player=Player.Black},
-                new ChessPiece{Pos=new Point(1, 1), Type=PieceType.Pawn, Player=Player.Black},
-                new ChessPiece{Pos=new Point(2, 1), Type=PieceType.Pawn, Player=Player.Black},
-                new ChessPiece{Pos=new Point(3, 1), Type=PieceType.Pawn, Player=Player.Black},
-                new ChessPiece{Pos=new Point(4, 1), Type=PieceType.Pawn, Player=Player.Black},
-                new ChessPiece{Pos=new Point(5, 1), Type=PieceType.Pawn, Player=Player.Black},
-                new ChessPiece{Pos=new Point(6, 1), Type=PieceType.Pawn, Player=Player.Black},
-                new ChessPiece{Pos=new Point(7, 1), Type=PieceType.Pawn, Player=Player.Black},
-                new ChessPiece{Pos=new Point(0, 0), Type=PieceType.Rook, Player=Player.Black},
-                new ChessPiece{Pos=new Point(1, 0), Type=PieceType.Knight, Player=Player.Black},
-                new ChessPiece{Pos=new Point(2, 0), Type=PieceType.Bishop, Player=Player.Black},
-                new ChessPiece{Pos=new Point(3, 0), Type=PieceType.King, Player=Player.Black},
-                new ChessPiece{Pos=new Point(4, 0), Type=PieceType.Queen, Player=Player.Black},
-                new ChessPiece{Pos=new Point(5, 0), Type=PieceType.Bishop, Player=Player.Black},
-                new ChessPiece{Pos=new Point(6, 0), Type=PieceType.Knight, Player=Player.Black},
-                new ChessPiece{Pos=new Point(7, 0), Type=PieceType.Rook, Player=Player.Black}
-            };
+            A1.Source = (System.Windows.Media.ImageSource)Resources["BlackBishop"];
+
         }
     }
 }
