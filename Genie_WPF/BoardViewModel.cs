c899541fa652856db51635805c8995b643ad406d
Terry malone <terry.malone@red-gate.com>
2022-01-16 20:56:48 +0000

    Removed redundant code

--- Genie_WPF/BoardViewModel.cs
+++ Genie_WPF/BoardViewModel.cs
@@ -9,61 +9,9 @@ namespace Genie_WPF
 {
     public class BoardViewModel : INotifyPropertyChanged
     {
-        private string _b6;
-        private string _c6;
 
         public BoardViewModel()
         {
-            B6 = "WhitePawn";
-            C6 = "BlackRook";
-
-            var chessBoard = (UniformGrid)App.Current.Windows[0].FindName("ChessBoard");
-        }
-
-        public string B6
-        {
-            get => _b6;
-
-            set
-            {
-                _b6 = value;
-                OnPropertyChanged("B6");
-            }
-        }
-
-        public string C6
-        {
-            get => _c6;
-
-            set
-            {
-                _c6 = value;
-                OnPropertyChanged("C6");
-            }
-        }
-
-
-        public BitmapImage GridImageA
-        {
-            get;
-
-            set;
-        }
-
-        private string _myTextBoxValue;
-        public string MyTextBoxValue
-        {
-            get => _myTextBoxValue;
-
-            set
-            {
-                _myTextBoxValue = value;
-                OnPropertyChanged("MyTextBoxValue");
-            }
-        }
-        public object FullGrid
-        {
-            get { throw new System.NotImplementedException(); }
         }
 
         public void AddPiece(object whitePawn, string position)
--- Genie_WPF/MainWindow.xaml.cs
+++ Genie_WPF/MainWindow.xaml.cs
@@ -21,137 +21,114 @@ namespace Genie_WPF
         {
             InitializeComponent();
 
-            InitialiseBitmaps();
-
             LookupTables.InitialiseAllTables();
             var boardState = FenTranslator.ToBoardState("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
             //var boardState = FenTranslator.ToBoardState("5r1k/4Qpq1/4p3/1p1p2P1/2p2P2/1p2P3/3P4/BK6 b - -");
 
             Board.ItemsSource = new ObservableCollection<ChessPiece>
-        {
-            new ChessPiece{Pos=new Point(0, 6), Type=PieceType.Pawn, Player=Player.White},
-            new ChessPiece{Pos=new Point(1, 6), Type=PieceType.Pawn, Player=Player.White},
-            new ChessPiece{Pos=new Point(2, 6), Type=PieceType.Pawn, Player=Player.White},
-            new ChessPiece{Pos=new Point(3, 6), Type=PieceType.Pawn, Player=Player.White},
-            new ChessPiece{Pos=new Point(4, 6), Type=PieceType.Pawn, Player=Player.White},
-            new ChessPiece{Pos=new Point(5, 6), Type=PieceType.Pawn, Player=Player.White},
-            new ChessPiece{Pos=new Point(6, 6), Type=PieceType.Pawn, Player=Player.White},
-            new ChessPiece{Pos=new Point(7, 6), Type=PieceType.Pawn, Player=Player.White},
-            new ChessPiece{Pos=new Point(0, 7), Type=PieceType.Rook, Player=Player.White},
-            new ChessPiece{Pos=new Point(1, 7), Type=PieceType.Knight, Player=Player.White},
-            new ChessPiece{Pos=new Point(2, 7), Type=PieceType.Bishop, Player=Player.White},
-            new ChessPiece{Pos=new Point(3, 7), Type=PieceType.King, Player=Player.White},
-            new ChessPiece{Pos=new Point(4, 7), Type=PieceType.Queen, Player=Player.White},
-            new ChessPiece{Pos=new Point(5, 7), Type=PieceType.Bishop, Player=Player.White},
-            new ChessPiece{Pos=new Point(6, 7), Type=PieceType.Knight, Player=Player.White},
-            new ChessPiece{Pos=new Point(7, 7), Type=PieceType.Rook, Player=Player.White},
-            new ChessPiece{Pos=new Point(0, 1), Type=PieceType.Pawn, Player=Player.Black},
-            new ChessPiece{Pos=new Point(1, 1), Type=PieceType.Pawn, Player=Player.Black},
-            new ChessPiece{Pos=new Point(2, 1), Type=PieceType.Pawn, Player=Player.Black},
-            new ChessPiece{Pos=new Point(3, 1), Type=PieceType.Pawn, Player=Player.Black},
-            new ChessPiece{Pos=new Point(4, 1), Type=PieceType.Pawn, Player=Player.Black},
-            new ChessPiece{Pos=new Point(5, 1), Type=PieceType.Pawn, Player=Player.Black},
-            new ChessPiece{Pos=new Point(6, 1), Type=PieceType.Pawn, Player=Player.Black},
-            new ChessPiece{Pos=new Point(7, 1), Type=PieceType.Pawn, Player=Player.Black},
-            new ChessPiece{Pos=new Point(0, 0), Type=PieceType.Rook, Player=Player.Black},
-            new ChessPiece{Pos=new Point(1, 0), Type=PieceType.Knight, Player=Player.Black},
-            new ChessPiece{Pos=new Point(2, 0), Type=PieceType.Bishop, Player=Player.Black},
-            new ChessPiece{Pos=new Point(3, 0), Type=PieceType.King, Player=Player.Black},
-            new ChessPiece{Pos=new Point(4, 0), Type=PieceType.Queen, Player=Player.Black},
-            new ChessPiece{Pos=new Point(5, 0), Type=PieceType.Bishop, Player=Player.Black},
-            new ChessPiece{Pos=new Point(6, 0), Type=PieceType.Knight, Player=Player.Black},
-            new ChessPiece{Pos=new Point(7, 0), Type=PieceType.Rook, Player=Player.Black}
-        };
-
-
-
+            {
+                new ChessPiece{Pos=new Point(0, 6), Type=PieceType.Pawn, Player=Player.White},
+                new ChessPiece{Pos=new Point(1, 6), Type=PieceType.Pawn, Player=Player.White},
+                new ChessPiece{Pos=new Point(2, 6), Type=PieceType.Pawn, Player=Player.White},
+                new ChessPiece{Pos=new Point(3, 6), Type=PieceType.Pawn, Player=Player.White},
+                new ChessPiece{Pos=new Point(4, 6), Type=PieceType.Pawn, Player=Player.White},
+                new ChessPiece{Pos=new Point(5, 6), Type=PieceType.Pawn, Player=Player.White},
+                new ChessPiece{Pos=new Point(6, 6), Type=PieceType.Pawn, Player=Player.White},
+                new ChessPiece{Pos=new Point(7, 6), Type=PieceType.Pawn, Player=Player.White},
+                new ChessPiece{Pos=new Point(0, 7), Type=PieceType.Rook, Player=Player.White},
+                new ChessPiece{Pos=new Point(1, 7), Type=PieceType.Knight, Player=Player.White},
+                new ChessPiece{Pos=new Point(2, 7), Type=PieceType.Bishop, Player=Player.White},
+                new ChessPiece{Pos=new Point(3, 7), Type=PieceType.King, Player=Player.White},
+                new ChessPiece{Pos=new Point(4, 7), Type=PieceType.Queen, Player=Player.White},
+                new ChessPiece{Pos=new Point(5, 7), Type=PieceType.Bishop, Player=Player.White},
+                new ChessPiece{Pos=new Point(6, 7), Type=PieceType.Knight, Player=Player.White},
+                new ChessPiece{Pos=new Point(7, 7), Type=PieceType.Rook, Player=Player.White},
+                new ChessPiece{Pos=new Point(0, 1), Type=PieceType.Pawn, Player=Player.Black},
+                new ChessPiece{Pos=new Point(1, 1), Type=PieceType.Pawn, Player=Player.Black},
+                new ChessPiece{Pos=new Point(2, 1), Type=PieceType.Pawn, Player=Player.Black},
+                new ChessPiece{Pos=new Point(3, 1), Type=PieceType.Pawn, Player=Player.Black},
+                new ChessPiece{Pos=new Point(4, 1), Type=PieceType.Pawn, Player=Player.Black},
+                new ChessPiece{Pos=new Point(5, 1), Type=PieceType.Pawn, Player=Player.Black},
+                new ChessPiece{Pos=new Point(6, 1), Type=PieceType.Pawn, Player=Player.Black},
+                new ChessPiece{Pos=new Point(7, 1), Type=PieceType.Pawn, Player=Player.Black},
+                new ChessPiece{Pos=new Point(0, 0), Type=PieceType.Rook, Player=Player.Black},
+                new ChessPiece{Pos=new Point(1, 0), Type=PieceType.Knight, Player=Player.Black},
+                new ChessPiece{Pos=new Point(2, 0), Type=PieceType.Bishop, Player=Player.Black},
+                new ChessPiece{Pos=new Point(3, 0), Type=PieceType.King, Player=Player.Black},
+                new ChessPiece{Pos=new Point(4, 0), Type=PieceType.Queen, Player=Player.Black},
+                new ChessPiece{Pos=new Point(5, 0), Type=PieceType.Bishop, Player=Player.Black},
+                new ChessPiece{Pos=new Point(6, 0), Type=PieceType.Knight, Player=Player.Black},
+                new ChessPiece{Pos=new Point(7, 0), Type=PieceType.Rook, Player=Player.Black}
+            };
 
             var boardViewModel = new BoardViewModel();
 
-            boardViewModel.MyTextBoxValue = "Test";
-
-            foreach (var whitePawnMove in BitboardOperations.SplitBoardToArray(boardState.WhitePawns))
-            {
-                AddPiece(Piece.WhitePawn, whitePawnMove);
-            }
-            
-            foreach (var whiteKnightMove in BitboardOperations.SplitBoardToArray(boardState.WhiteKnights))
-            {
-                AddPiece(Piece.WhiteKnight, whiteKnightMove);
-            }
-            
-            foreach (var whiteBishopMove in BitboardOperations.SplitBoardToArray(boardState.WhiteBishops))
-            {
-                AddPiece(Piece.WhiteBishop, whiteBishopMove);
-            }
-            
-            foreach (var whiteRookMove in BitboardOperations.SplitBoardToArray(boardState.WhiteRooks))
-            {
-                AddPiece(Piece.WhiteRook, whiteRookMove);
-            }
-            
-            foreach (var whiteQueenMove in BitboardOperations.SplitBoardToArray(boardState.WhiteQueen))
-            {
-                AddPiece(Piece.WhiteQueen, whiteQueenMove);
-            }
-            
-            foreach (var whiteKingMove in BitboardOperations.SplitBoardToArray(boardState.WhiteKing))
-            {
-                AddPiece(Piece.WhiteKing, whiteKingMove);
-            }
-            
-            foreach (var blackPawnMove in BitboardOperations.SplitBoardToArray(boardState.BlackPawns))
-            {
-                AddPiece(Piece.BlackPawn, blackPawnMove);
-            }
-            
-            foreach (var blackKnightMove in BitboardOperations.SplitBoardToArray(boardState.BlackKnights))
-            {
-                AddPiece(Piece.BlackKnight, blackKnightMove);
-            }
-            
-            foreach (var blackBishopMove in BitboardOperations.SplitBoardToArray(boardState.BlackBishops))
-            {
-                AddPiece(Piece.BlackBishop, blackBishopMove);
-            }
-            
-            foreach (var blackRookMove in BitboardOperations.SplitBoardToArray(boardState.BlackRooks))
-            {
-                AddPiece(Piece.BlackRook, blackRookMove);
-            }
-            
-            foreach (var blackQueenMove in BitboardOperations.SplitBoardToArray(boardState.BlackQueen))
-            {
-                AddPiece(Piece.BlackQueen, blackQueenMove);
-            }
-            
-            foreach (var blackKingMove in BitboardOperations.SplitBoardToArray(boardState.BlackKing))
-            {
-                AddPiece(Piece.BlackKing, blackKingMove);
-            }
+            // foreach (var whitePawnMove in BitboardOperations.SplitBoardToArray(boardState.WhitePawns))
+            // {
+            //     AddPiece(Piece.WhitePawn, whitePawnMove);
+            // }
+            //
+            // foreach (var whiteKnightMove in BitboardOperations.SplitBoardToArray(boardState.WhiteKnights))
+            // {
+            //     AddPiece(Piece.WhiteKnight, whiteKnightMove);
+            // }
+            //
+            // foreach (var whiteBishopMove in BitboardOperations.SplitBoardToArray(boardState.WhiteBishops))
+            // {
+            //     AddPiece(Piece.WhiteBishop, whiteBishopMove);
+            // }
+            //
+            // foreach (var whiteRookMove in BitboardOperations.SplitBoardToArray(boardState.WhiteRooks))
+            // {
+            //     AddPiece(Piece.WhiteRook, whiteRookMove);
+            // }
+            //
+            // foreach (var whiteQueenMove in BitboardOperations.SplitBoardToArray(boardState.WhiteQueen))
+            // {
+            //     AddPiece(Piece.WhiteQueen, whiteQueenMove);
+            // }
+            //
+            // foreach (var whiteKingMove in BitboardOperations.SplitBoardToArray(boardState.WhiteKing))
+            // {
+            //     AddPiece(Piece.WhiteKing, whiteKingMove);
+            // }
+            //
+            // foreach (var blackPawnMove in BitboardOperations.SplitBoardToArray(boardState.BlackPawns))
+            // {
+            //     AddPiece(Piece.BlackPawn, blackPawnMove);
+            // }
+            //
+            // foreach (var blackKnightMove in BitboardOperations.SplitBoardToArray(boardState.BlackKnights))
+            // {
+            //     AddPiece(Piece.BlackKnight, blackKnightMove);
+            // }
+            //
+            // foreach (var blackBishopMove in BitboardOperations.SplitBoardToArray(boardState.BlackBishops))
+            // {
+            //     AddPiece(Piece.BlackBishop, blackBishopMove);
+            // }
+            //
+            // foreach (var blackRookMove in BitboardOperations.SplitBoardToArray(boardState.BlackRooks))
+            // {
+            //     AddPiece(Piece.BlackRook, blackRookMove);
+            // }
+            //
+            // foreach (var blackQueenMove in BitboardOperations.SplitBoardToArray(boardState.BlackQueen))
+            // {
+            //     AddPiece(Piece.BlackQueen, blackQueenMove);
+            // }
+            //
+            // foreach (var blackKingMove in BitboardOperations.SplitBoardToArray(boardState.BlackKing))
+            // {
+            //     AddPiece(Piece.BlackKing, blackKingMove);
+            // }
         }
 
-        private void AddPiece(Piece piece, ulong pieceMove)
-        {
-            var position = TranslationHelper.GetSquareNotation(pieceMove).ToUpper();
-            var control = (Image) this.FindName(position);
-            control.Source = _pieceBitmaps[piece];
-        }
-
-        private void InitialiseBitmaps()
-        {
-            _pieceBitmaps.Add(Piece.WhitePawn, (BitmapImage) Resources["WhitePawn"]);
-            _pieceBitmaps.Add(Piece.WhiteKnight, (BitmapImage) Resources["WhiteKnight"]);
-            _pieceBitmaps.Add(Piece.WhiteBishop, (BitmapImage) Resources["WhiteBishop"]);
-            _pieceBitmaps.Add(Piece.WhiteRook, (BitmapImage) Resources["WhiteRook"]);
-            _pieceBitmaps.Add(Piece.WhiteQueen, (BitmapImage) Resources["WhiteQueen"]);
-            _pieceBitmaps.Add(Piece.WhiteKing, (BitmapImage) Resources["WhiteKing"]);
-            _pieceBitmaps.Add(Piece.BlackPawn, (BitmapImage) Resources["BlackPawn"]);
-            _pieceBitmaps.Add(Piece.BlackBishop, (BitmapImage) Resources["BlackBishop"]);
-            _pieceBitmaps.Add(Piece.BlackKnight, (BitmapImage) Resources["BlackKnight"]);
-            _pieceBitmaps.Add(Piece.BlackRook, (BitmapImage) Resources["BlackRook"]);
-            _pieceBitmaps.Add(Piece.BlackQueen, (BitmapImage) Resources["BlackQueen"]);
-            _pieceBitmaps.Add(Piece.BlackKing, (BitmapImage) Resources["BlackKing"]);
-        }
+        // private void AddPiece(Piece piece, ulong pieceMove)
+        // {
+        //     var position = TranslationHelper.GetSquareNotation(pieceMove).ToUpper();
+        //     var control = (Image) this.FindName(position);
+        //     control.Source = _pieceBitmaps[piece];
+        // }
     }
 }
