bc6eb7a716b3fba7ed326dd64d173310aff6c757
Terry Malone <terry.malone@red-gate.com>
2022-01-24 16:44:06 +0000

    Can only select correct colour. Remove pieces

--- Genie_WPF/BoardViewModel.cs
+++ Genie_WPF/BoardViewModel.cs
@@ -12,14 +12,11 @@ namespace Genie_WPF
     public sealed class BoardViewModel : ViewModelBase
     {
         private readonly Game _game;
-        private BoardState _boardState;
 
         private ChessPiece _selectedPiece;
 
         private ObservableCollection<ChessPiece> _chessPieces;
         private string _fenPosition;
-
-
 
         public RelayCommand SetFenButtonClickCommand { get; }
 
@@ -56,16 +53,15 @@ namespace Genie_WPF
 
 
             _game = game;
-            _boardState = _game.GetCurrentBoardState();
 
             SetPieces();
         }
 
-        internal void SetBoard(object obj = null)
+        private void SetBoard(object obj = null)
         {
             try
             {
-                _boardState = FenTranslator.ToBoardState(_fenPosition);
+                _game.SetPosition(_fenPosition);
 
                 SetPieces();
             }
@@ -78,24 +74,40 @@ namespace Genie_WPF
         
         public void BoardClicked(int column, int row)
         {
-            var clickedPiece = _chessPieces.SingleOrDefault(p => p.Pos.X == column && p.Pos.Y == row);
+            var currentPlayer = _game.GetCurrentBoardState().WhiteToMove ? Player.White : Player.Black;
 
-            if (clickedPiece != null)
+
+            if (_selectedPiece is not null) // A piece has been selected. Deal with the move to
             {
-                if (_selectedPiece != null)
+               // var validMoves = _game.GetCurrentBoardState()
+
+                // Remove any existing piece
+                var pieceToRemove = _chessPieces.SingleOrDefault(p => p.Pos.X == column && p.Pos.Y == row);
+
+                if (pieceToRemove is not null)
                 {
-                    _selectedPiece.IsSelected = false;
+                    _chessPieces.Remove(pieceToRemove);
                 }
 
-                _selectedPiece = clickedPiece;
-                _selectedPiece.IsSelected = true;
-            }
-            else if (_selectedPiece != null)
-            {
                 _selectedPiece.Pos = new Point(column, row);
 
                 _selectedPiece.IsSelected = false;
                 _selectedPiece = null;
+            }
+            else
+            {
+                var clickedPiece = _chessPieces.SingleOrDefault(p => p.Pos.X == column && p.Pos.Y == row && p.Player == currentPlayer);
+
+                if (clickedPiece is not null)
+                {
+                    if (_selectedPiece != null)
+                    {
+                        _selectedPiece.IsSelected = false;
+                    }
+
+                    _selectedPiece = clickedPiece;
+                    _selectedPiece.IsSelected = true;
+                }
             }
         }
 
@@ -103,67 +115,69 @@ namespace Genie_WPF
         {
             _chessPieces.Clear();
 
-            foreach (var whitePawnMove in BitboardOperations.SplitBoardToArray(_boardState.WhitePawns))
+            var boardState = _game.GetCurrentBoardState();
+
+            foreach (var whitePawnMove in BitboardOperations.SplitBoardToArray(boardState.WhitePawns))
             {
                 AddPiece(Player.White, PieceType.Pawn, whitePawnMove);
             }
             
-            foreach (var whiteKnightMove in BitboardOperations.SplitBoardToArray(_boardState.WhiteKnights))
+            foreach (var whiteKnightMove in BitboardOperations.SplitBoardToArray(boardState.WhiteKnights))
             {
                 AddPiece(Player.White, PieceType.Knight, whiteKnightMove);
             }
             
-            foreach (var whiteBishopMove in BitboardOperations.SplitBoardToArray(_boardState.WhiteBishops))
+            foreach (var whiteBishopMove in BitboardOperations.SplitBoardToArray(boardState.WhiteBishops))
             {
                 AddPiece(Player.White, PieceType.Bishop, whiteBishopMove);
             }
             
-            foreach (var whiteRookMove in BitboardOperations.SplitBoardToArray(_boardState.WhiteRooks))
+            foreach (var whiteRookMove in BitboardOperations.SplitBoardToArray(boardState.WhiteRooks))
             {
                 AddPiece(Player.White, PieceType.Rook, whiteRookMove);
             }
             
-            foreach (var whiteQueenMove in BitboardOperations.SplitBoardToArray(_boardState.WhiteQueen))
+            foreach (var whiteQueenMove in BitboardOperations.SplitBoardToArray(boardState.WhiteQueen))
             {
                 AddPiece(Player.White, PieceType.Queen, whiteQueenMove);
             }
             
-            foreach (var whiteKingMove in BitboardOperations.SplitBoardToArray(_boardState.WhiteKing))
+            foreach (var whiteKingMove in BitboardOperations.SplitBoardToArray(boardState.WhiteKing))
             {
                 AddPiece(Player.White, PieceType.King, whiteKingMove);
             }
             
-            foreach (var blackPawnMove in BitboardOperations.SplitBoardToArray(_boardState.BlackPawns))
+            foreach (var blackPawnMove in BitboardOperations.SplitBoardToArray(boardState.BlackPawns))
             {
                 AddPiece(Player.Black, PieceType.Pawn, blackPawnMove);
             }
             
-            foreach (var blackKnightMove in BitboardOperations.SplitBoardToArray(_boardState.BlackKnights))
+            foreach (var blackKnightMove in BitboardOperations.SplitBoardToArray(boardState.BlackKnights))
             {
                 AddPiece(Player.Black, PieceType.Knight, blackKnightMove);
             }
             
-            foreach (var blackBishopMove in BitboardOperations.SplitBoardToArray(_boardState.BlackBishops))
+            foreach (var blackBishopMove in BitboardOperations.SplitBoardToArray(boardState.BlackBishops))
             {
                 AddPiece(Player.Black, PieceType.Bishop, blackBishopMove);
             }
             
-            foreach (var blackRookMove in BitboardOperations.SplitBoardToArray(_boardState.BlackRooks))
+            foreach (var blackRookMove in BitboardOperations.SplitBoardToArray(boardState.BlackRooks))
             {
                 AddPiece(Player.Black, PieceType.Rook, blackRookMove);
             }
             
-            foreach (var blackQueenMove in BitboardOperations.SplitBoardToArray(_boardState.BlackQueen))
+            foreach (var blackQueenMove in BitboardOperations.SplitBoardToArray(boardState.BlackQueen))
             {
                 AddPiece(Player.Black, PieceType.Queen, blackQueenMove);
             }
             
-            foreach (var blackKingMove in BitboardOperations.SplitBoardToArray(_boardState.BlackKing))
+            foreach (var blackKingMove in BitboardOperations.SplitBoardToArray(boardState.BlackKing))
             {
                 AddPiece(Player.Black, PieceType.King, blackKingMove);
             }
 
-            FenPosition = FenTranslator.ToFenString(_boardState);
+            FenPosition = FenTranslator.ToFenString(boardState);
         }
 
         private void AddPiece(Player player, PieceType pieceType, ulong pieceMove)
@@ -181,7 +195,7 @@ namespace Genie_WPF
 
         public void GetFen(object obj = null)
         {
-            FenPosition = FenTranslator.ToFenString(_boardState);
+            FenPosition = FenTranslator.ToFenString(_game.GetCurrentBoardState());
         }
     }
 }
