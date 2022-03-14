8966edf4098ad21f3fcf8f741856001224297af3
Terry Malone <terry.malone@red-gate.com>
2022-03-14 13:18:37 +0000

    Add another test

--- ChessEngineTests/MoveSearching/MoveOrderingTests.cs
+++ ChessEngineTests/MoveSearching/MoveOrderingTests.cs
@@ -10,6 +10,32 @@ namespace ChessEngineTests.MoveSearching;
 public class MoveOrderingTests
 {
     [Test]
+    public void NoCapturesMakesNoChanges()
+    {
+        var board = new Board();
+        board.SetPosition("3kq3/2npp3/5b2/8/8/8/3B1PPP/4R1K1 w - - 0 1");
+
+        var pieceMoves = new List<PieceMove>
+        {
+            new() { Position = 8192u, Moves = 2097152u, Type = PieceType.Pawn, SpecialMove = SpecialMoveType.Normal },           // pawn move
+            new() { Position = 8192u, Moves = 536870912u, Type = PieceType.Pawn, SpecialMove = SpecialMoveType.DoublePawnPush }, // pawn double move
+            new() { Position = 64u, Moves = 32u, Type = PieceType.King, SpecialMove = SpecialMoveType.Normal },                  // King move
+            new() { Position = 16u, Moves = 17592186044416u, Type = PieceType.Rook, SpecialMove = SpecialMoveType.Normal },      // Rook move
+            new() { Position = 65536u, Moves = 17179869184u, Type = PieceType.Queen, SpecialMove = SpecialMoveType.Normal },      // Queen move
+            new() { Position = 8388608u, Moves = 274877906944u, Type = PieceType.Knight, SpecialMove = SpecialMoveType.Normal },      // Knight move
+            new() { Position = 2048u, Moves = 4294967296u, Type = PieceType.Bishop, SpecialMove = SpecialMoveType.Normal },      // Bishop move
+        };
+
+        Assert.That(pieceMoves[0].Moves, Is.EqualTo(2097152u));
+        Assert.That(pieceMoves[1].Moves, Is.EqualTo(536870912u));
+        Assert.That(pieceMoves[2].Moves, Is.EqualTo(32u));
+        Assert.That(pieceMoves[3].Moves, Is.EqualTo(17592186044416u));
+        Assert.That(pieceMoves[4].Moves, Is.EqualTo(17179869184u));
+        Assert.That(pieceMoves[5].Moves, Is.EqualTo(274877906944u));
+        Assert.That(pieceMoves[6].Moves, Is.EqualTo(4294967296u));
+    }
+
+    [Test]
     public void SamePieceCapturingDifferentPieces()
     {
         var board = new Board();
@@ -66,25 +92,5 @@ public class MoveOrderingTests
         Assert.That(pieceMoves[5].Moves, Is.EqualTo(1048576u));
         Assert.That(pieceMoves[6].Position, Is.EqualTo(268435456u));  // King move
         Assert.That(pieceMoves[6].Moves, Is.EqualTo(68719476736u));
-    }
-
-    [Test]
-    public void METHOD()
-    {
-        var board = new Board();
-        board.SetPosition("4k3/R3p3/8/6B1/8/3q4/4P3/4K3 w - - 0 1");
-
-        var pieceMoves = new List<PieceMove>
-        {
-            new() { Position = 274877906944u, Moves = 9007199254740992u, Type = PieceType.Bishop, SpecialMove = SpecialMoveType.Capture }, // Bishop capturing pawn
-            new() { Position = 4096u, Moves = 524288u, Type = PieceType.Pawn, SpecialMove = SpecialMoveType.Capture }, // Pawn capturing queen
-            new() { Position = 281474976710656u, Moves = 9007199254740992u, Type = PieceType.Rook, SpecialMove = SpecialMoveType.Capture } // Rook capturing pawn
-        };
-
-        MoveOrdering.OrderMovesByMvvLva(board, pieceMoves);
-
-        Assert.That(pieceMoves[0].Type, Is.EqualTo(PieceType.Pawn));
-        Assert.That(pieceMoves[1].Type, Is.EqualTo(PieceType.Bishop));
-        Assert.That(pieceMoves[2].Type, Is.EqualTo(PieceType.Rook));
     }
 }
